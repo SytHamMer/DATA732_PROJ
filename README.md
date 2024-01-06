fde79f605693b8583731144722f9a625e2841210
SytHamMer <mathys.lebon1@gmail.com>
2024-01-06 14:45:46 +0100

    [UPDATE]:readme, svg background, clean files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # School Project
 ## DATA ANALYSE AND VISUALISATION
+
+
+### Comment lancer le dashboard
+
+Il suffit de lancer le fichier python dashboard.py et d'ouvrir l'adresse url qui s'affichera dans le terminal. 
+Elle devrait être similaire à http://127.0.0.1:8050/
+
+Penser à vider le cache avant ceci peut poser des problèmes. 
+Ainsi que de bien avoir le zoom de la fenêtre à 100%.
+
+
+Pour l'instant il est impossible de zoomer sur l'image svg du dashboard, ce problème n'a pour l'instant pas de solution efficace. 
+Vous pouvez tout de fois ouvrir ces images dans un autre onglet et donc les voirs en plus grand.
+ 
--- dashboard.py
+++ dashboard.py
@@ -1,135 +1,180 @@
-import dash
-from dash import dcc, html
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+import dash
+from dash import html,dcc
+from dash.dependencies import Input,Output
 import json
 
-# DATA
+app = dash.Dash(__name__, external_stylesheets=['assets/styles.css'])
+# app = dash.Dash(__name__, external_stylesheets=['assets/styles.css'], suppress_callback_exceptions=True)
 
-external_stylesheets =  ['https://codepen.io/chriddyp/pen/bWLwgP.css'] 
+#IMPORT DATA
 
-#CSS
+first_df = pd.read_csv("PeopleSaves/MALI_ER_Top10_People.csv")
+names = list(first_df["Names"])
+options = [{'label': name, 'value': name} for name in names]
+COLORS = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'white', 'cyan']
 
-colors = {'background' : '#111111',
-          'text':"#7FDBFF"}
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    html.H1("Dashboard"),
 
-    # Choix journal
-    html.Div([
-        dcc.Dropdown(id='dropdown_1',
-                     options=[{'label': "MALI_ER","value":"MALI_ER"},
-                              {'label': "MALI_FP","value":"MALI_FP"},
-                              {'label': "MALI_SN","value":"MALI_SN"},
-                              {'label': "FRANCE_FdS","value":"FRANCE_FdS"},
-                              {'label': "FRANCE_SN","value":"FRANCE_SN"},
-                              {'label': "FRANCE_ER","value":"FRANCE_ER"},
-                              {'label': "FRANCE_FP","value":"FRANCE_FP"}
-                              ],
-                     value='MALI_ER')  
+
+#APP LAYOUT
+
+app.layout = html.Div(id="main_div",
+                      children=[
+    html.Div(id="title_div",children=[
+            html.H1("DASHBOARD", id="title"),    
     ]),
+    html.Div(id="dropdown_div",children=[
+        dcc.Dropdown(id="select_journal",
+                 options=[
+                     {'label': "MALI_ER","value":"MALI_ER"},
+                     {'label': "MALI_FP","value":"MALI_FP"},
+                     {'label': "MALI_SN","value":"MALI_SN"},
+                     {'label': "FRANCE_FdS","value":"FRANCE_FdS"},
+                     {'label': "FRANCE_SN","value":"FRANCE_SN"},
+                     {'label': "FRANCE_ER","value":"FRANCE_ER"},
+                     {'label': "FRANCE_FP","value":"FRANCE_FP"}
+                     ],
+                 value='MALI_ER',
+                 multi=False,
+                 ),
+        dcc.Dropdown(id="select_person",
+                 options=options,
+                 value = options[0]["value"],
 
-    # Choix personne
-    html.Div([
-        dcc.Dropdown(id='dropdown_2',
-                     options=[], 
-                     value='')      
-    ]),
-    html.Div([
-        html.H2("CACA"),  # Ajoutez votre titre au-dessus de l'image
-        html.Img(src='/gephi/FRANCE_FdS.svg', id='svg-image')  # Ajoutez l'image SVG
-    ]),
-    
-    # Histogramme 2D
-    dcc.Graph(id='bar'),
-
-    # Line chart
-    dcc.Graph(id='line-chart'),
-
-    # Carte
-    dcc.Graph(id='map', config={'scrollZoom': False}),
-    
-    
-    ])
-
-
-
-#callback menu deroulant
-@app.callback(
-    dash.dependencies.Output('dropdown_2', 'options'),
-    [dash.dependencies.Input('dropdown_1', 'value')]
-)
-def update_2_dropdown(selected_journal):
-    # Votre logique pour mettre à jour le dropdown 2 en fonction de dropdown 1
-    dfDictPers = pd.read_csv(f"PeopleSaves/{selected_journal}_Top10_People.csv")
-    names = list(dfDictPers["Names"])
-    options = [{'label': name, 'value': name} for name in names]
-    return options
-
-@app.callback(
-    dash.dependencies.Output('bar', 'figure'),
-    dash.dependencies.Output('line-chart', 'figure'),
-    [dash.dependencies.Input('dropdown_1', 'value')]
-)
-def update_bar_line(selected_dropdown_value):
-    
-    bar_df = pd.read_csv(f"PeopleSaves/{selected_dropdown_value}_Top10_People.csv")
-    bar_df_sorted = bar_df.sort_values(by='Values', ascending=False)
-    bar_fig = px.bar(bar_df_sorted, x='Names', y='Values', labels={'Names': 'Nom', 'Values': 'Valeur'}, title='Histogramme des 10 personnes les plus fréquentes')
-
-    with open(f"PeopleSaves/{selected_dropdown_value}_Top10_People_Evolution.json",'r') as f:
-        line_dict = json.load(f)
+                 ),
         
+        
+    ]),
+    
+    html.Div(id="all_graphs",children=[
+        html.Div(id="left_graphs",children=[
+                dcc.Graph(id='map',figure={}),
+                # dcc.Graph(id="barchart_top10",figure={}),
+                dcc.Graph(id='linechart_top10',figure={}),
+                ]),
+        html.Div(id="right_graphs",children=[
+        # dcc.Graph(id='linechart_top10',figure={}),
+        dcc.Graph(id="barchart_top10",figure={}),
+        html.Div(id="svg_div")
+        ]),
+    ]),
+
+    ]
+)
+
+
+
+# CALLBACKS
+@app.callback(
+    [Output(component_id="select_person",component_property='options'),
+     Output(component_id="select_person",component_property='value'),
+     Output(component_id="barchart_top10",component_property='figure'),
+     Output(component_id="linechart_top10",component_property='figure'),
+     Output(component_id="svg_div",component_property='children')],
+    [Input(component_id="select_journal",component_property="value")]
+)
+
+def update_top_10(option_selected):
+    options_df = pd.read_csv(f"PeopleSaves/{option_selected}_Top10_People.csv")
+    names = list(options_df["Names"])
+    pers_options = [{'label': name, 'value': name} for name in names]
+    value = pers_options[0]["value"]
+    bar_fig_df = pd.read_csv(f"PeopleSaves/{option_selected}_Top10_People.csv")
+    bar_fig_df_sorted = bar_fig_df.sort_values(by='Values', ascending=False)
+    bar_title = f"Histogramme des 10 personnes les plus citées dans le journal {option_selected}"
     
     
+    color_dict = dict(zip(names, COLORS[:len(names)]))
+    bar_fig = px.bar(bar_fig_df_sorted,
+                     x='Names', 
+                     y='Values', 
+                     labels={'Names': 'Nom', 'Values': 'Valeur'},
+                     title=bar_title,
+                     template='plotly_dark',
+                     color = 'Names',
+                     color_discrete_map=color_dict)
     
-    line_chart_fig = None
-    for key, values in line_dict.items():
+    with open(f"PeopleSaves/{option_selected}_Top10_People_Evolution.json",'r') as f:
+        line_dict = json.load(f)
+    line_fig= None
+    # print(line_dict)
+    for index, key in enumerate(line_dict.keys()):
+        values = line_dict[key]
         dates = []
         values_list = []
         for entry in values:
             for date, value in entry.items():
                 dates.append(date)
                 values_list.append(value)
-        if line_chart_fig is None:
-            line_chart_fig = px.line(labels={'x': 'Date', 'y': 'Valeur'}, title='Evolution de la présence')
-            line_chart_fig.add_scatter(x=dates, y=values_list, mode='lines', name=key)
+        if line_fig is None:
+            line_fig = px.line(labels={'x': 'Date', 'y': 'Valeur'}, 
+                               title="Evolution de l'apparition des 10 personnes au fil du temps",
+                               template="plotly_dark",
+                               )
+            line_fig.add_scatter(x=dates, y=values_list, mode='lines', name=key,line_color = COLORS[index])
         else:
-            line_chart_fig.add_scatter(x=dates, y=values_list, mode='lines', name=key)
-    return bar_fig, line_chart_fig
+            line_fig.add_scatter(x=dates, y=values_list, mode='lines', name=key,line_color = COLORS[index])
+    
+    svg_file = f"gephi/{option_selected}.svg"
+    graph_title = f"Graphe relationelle des personnes se trouvant dans les articles de {option_selected}"
+    child = [html.H3(graph_title, id="graph_title"),
+            html.Div(id="svg_child_div",children=[
+            html.Img(id="svg_image", src=dash.get_asset_url(svg_file)),
+        ])]
+         
+    #return Output1 and Output2
+    return pers_options,value, bar_fig,line_fig,child
+
+
+
+
+
 
 @app.callback(
-    dash.dependencies.Output('map', 'figure'),
-    [dash.dependencies.Input('dropdown_1', 'value'),
-     dash.dependencies.Input('dropdown_2', 'value')]
+    Output(component_id="map",component_property='figure'),
+    [Input(component_id="select_person",component_property="value"),
+     Input(component_id="select_journal",component_property="value")]
 )
-def update_map(selected_dropdown_value_1, selected_dropdown_value_2):
-    #use both parameter in order to create the mapping value_1 = journal value_2 = personne
-    with open(f"CountriesSaves/{selected_dropdown_value_1}_top10_contries.json",'r')as f:
+def update_map(person_selected,journal_selected):
+    with open(f"CountriesSaves/{journal_selected}_top10_contries.json",'r')as f:
         contries_data = json.load(f)
-    data_pers = contries_data[selected_dropdown_value_2]
-    df_pers = pd.DataFrame(data_pers)
-    min_value = df_pers['Value'].min()
-    max_value = df_pers['Value'].min()
-    map_fig = px.choropleth(df_pers,
-                            locations="ISO-3", 
-                            locationmode="ISO-3",
-                            color="Value",  
-                            color_continuous_scale="YlOrRd",  
-                            range_color=(min_value, max_value),  
-                            hover_name="Loc",  
-                            projection="natural earth"  
-                        )
+    # print(person_selected)
+    #Cas ou y'a rien de séléctionner
+    if type(person_selected) == int:
+        #cas chargement de la page
+        data_pers= contries_data["macron"]
+        title = f"Carte des pays en lien avec macron dans les articles du journal  {journal_selected}."
+    else:
+        title = f"Carte des pays en lien avec {person_selected} dans les articles du journal  {journal_selected}."
+        data_pers = contries_data[person_selected]
+    df_map = pd.DataFrame(data_pers)    
+    min_value  = df_map["Value"].min()
+    max_value = df_map["Value"].max()
+    fig = px.choropleth(
+        data_frame=df_map,
+        locationmode="ISO-3",
+        locations="ISO-3",
+        color="Value",
+        hover_data=["Loc","Value"],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        range_color=(min_value,max_value),
+        template="plotly_dark"
+    )
+    fig.update_layout(
+        title_text=title,
+        title_x=0.5,  
+
+    )
+    return fig
 
 
-    map_fig.update_geos(showcoastlines=True, coastlinecolor="Black", coastlinewidth=2)
-
-    map_fig.update_layout(title_text=f'Heatmap of countries in {selected_dropdown_value_1} for {selected_dropdown_value_2}')  # Set the title
-    return map_fig
 
 
+
+
+    
 if __name__ == '__main__':
         
     app.run_server(debug=True)
